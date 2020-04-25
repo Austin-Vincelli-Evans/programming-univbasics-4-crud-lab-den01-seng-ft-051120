3c3010607bcb9d1468e28722e971d242d0ef48d7
Austin Evans <unity7591@gmail.com>
2020-04-25 20:02:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,4 @@
+
 def create_an_empty_array
   create_an_empty_array = []
 end
@@ -7,28 +8,27 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_empty_array.push (element)
+  create_an_array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_empty_array.unshift (element)
+  create_an_array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_empty_array.pop
-  create_an_array
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_empty_array.shift
+  create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_empty_array[2]
+  create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array(0)
+  create_an_array[0]
   
 end
 
@@ -37,5 +37,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-create_an_empty_array[1]=(element)
+  create_an_array[1]=(element)
 end
